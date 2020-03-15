289e61e7116d02ade1b6901db2f050b959a33512
YONATAN SHOSHANI <yonatanshoshani7@gmail.com>
2020-03-15 09:31:22 +0200

    added validators

--- dataproject/dataproject/static/Models/QueryFormStructure.py
+++ dataproject/dataproject/static/Models/QueryFormStructure.py
@@ -9,14 +9,14 @@ from wtforms import validators, ValidationError
 from wtforms.validators import DataRequired
 
 class LoginFormStructure(FlaskForm):
-    username   = StringField('User name:  ' , validators = [DataRequired()])
-    password   = PasswordField('Pass word:  ' , validators = [DataRequired()])
+    username   = StringField('User name:  ' , validators = [DataRequired("please enter your first name")])
+    password   = PasswordField('Pass word:  ' , validators = [DataRequired("please enter your password")])
     submit = SubmitField('Submit')
 
 
 class UserRegistrationFormStructure(FlaskForm):
-    FirstName  = StringField('First name:  ' , validators = [DataRequired()])
-    LastName   = StringField('Last name:  ' , validators = [DataRequired()])
+    FirstName  = StringField('First name:  ' , validators = [DataRequired("please enter your first name")])
+    LastName   = StringField('Last name:  ' , validators = [DataRequired("please enter your last name")])
     PhoneNum   = StringField('Phone number:  ' , validators = [DataRequired()])
     EmailAddr  = StringField('E-Mail:  ' , validators = [DataRequired()])
     username   = StringField('User name:  ' , validators = [DataRequired()])
